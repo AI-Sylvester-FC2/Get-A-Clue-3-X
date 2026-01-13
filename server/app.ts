cc754b20a2dcb57c721e48150f701f1276284d1c
Katy Wells <katy.wells@verizon.net>
2026-01-13 15:47:07 -0500

    fix gitignore

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,14 @@
 import express, { ErrorRequestHandler } from 'express';
 import cors from 'cors';
-import 'dotenv/config'; 
+import 'dotenv/config';
 import { ServerError } from './types.js';
 
 const app = express();
 
 console.log('Express app created');
 
-
 app.use(cors());
 app.use(express.json());
-
-
 
 // Routes
 app.get('/', (req, res) => {
@@ -23,7 +20,6 @@ app.get('/test', (req, res) => {
   console.log('TEST ROUTE HIT!');
   res.json({ test: 'This is a test route' });
 });
-
 
 // ... rest of your code (error handler)
 const errorHandler: ErrorRequestHandler = (
