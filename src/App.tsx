eafb31e38f63be09280f66ce512d488d25b5f500
Xavier <xglover410@gmail.com>
2026-01-13 11:34:29 -0500

    InputAndAiStory

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import CharacterBox from "./components/CharacterBox"
+import InputAndAiStory from "./components/InputAndAiStory"
 
 export default function App() {
   return (
     <>
      <h1>Clue Game</h1>
+      <InputAndAiStory />
       < CharacterBox />
-    
-     
     </>
     
   )
